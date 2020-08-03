a30f70579cd28678dca4f5783a00bf89bbe62b33
MoeinZ <moein459@gmail.com>
2020-08-03 23:02:09 +0430

    minor changes in sidebar and docs project

--- projects/docs/src/app/app.module.ts
+++ projects/docs/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {KButtonModule} from 'ngx-k-components/button';
 import {KAlertModule} from 'ngx-k-components/alert';
 import {KBadgeModule} from 'ngx-k-components/badge';
 import {KCardModule} from 'ngx-k-components/card';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {KFormModule} from '../../../components/form/src/k-form.module';
-import {KNavbarModule} from '../../../components/navbar/src/k-navbar.module';
-import {KSidebarModule} from '../../../components/sidebar/src/k-sidebar.module';
-import {SidebarStatus} from "../../../components/sidebar/src/models/sidebar-status.enum";
-import {SidebarMode} from "../../../components/sidebar/src/models/sidebar-mode.enum";
+import {KSidebarModule, SidebarMode, SidebarStatus} from "ngx-k-components/sidebar";
+import {KFormModule} from "ngx-k-components/form";
+import {KNavbarModule} from "ngx-k-components/navbar";
 
 @NgModule({
 	declarations: [
