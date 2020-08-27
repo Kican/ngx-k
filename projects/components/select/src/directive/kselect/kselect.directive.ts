3589fd821f7bd5311cb0d7cea790fd279cdd932a
mo3in <moein.hente@gmail.com>
2020-08-28 01:57:23 +0430

    select value data-provider

--- projects/components/select/src/directive/kselect/kselect.directive.ts
+++ projects/components/select/src/directive/kselect/kselect.directive.ts
@@ -1,15 +1,33 @@
-import {Directive, Input} from '@angular/core';
+import {Directive, EventEmitter, Input, Output} from '@angular/core';
+import {SelectDataProvider} from '../../provider/select-data-provider';
 
 @Directive({
 	selector: '[k-select]'
 })
 export class KSelectDirective {
-
 	@Input()
 	bindValue: string;
 
 	@Input()
 	bindLabel: string;
+
+	@Input()
+	items: any[];
+
+	@Input('data-provider')
+	dataProvider: SelectDataProvider<any>;
+
+	@Input()
+	multiple: boolean = false;
+
+	@Output()
+	add: EventEmitter<any>;
+
+	@Output()
+	change: EventEmitter<any>;
+
+	@Output()
+	remove: EventEmitter<any>;
 
 	constructor() {
 	}
---
+++ projects/components/select/src/provider/value-select-data-provider.spec.ts
@@ -0,0 +1,7 @@
+import { ValueSelectDataProvider } from './value-select-data-provider';
+
+describe('ValueSelectDataProvider', () => {
+  it('should create an instance', () => {
+    expect(new ValueSelectDataProvider()).toBeTruthy();
+  });
+});
