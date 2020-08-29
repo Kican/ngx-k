179ceaa80594da66597cc02a6c8a17152b714c47
mo3in <moein.hente@gmail.com>
2020-08-30 01:00:32 +0430

    some fixes in ci

--- libs-publish.js
+++ libs-publish.js
@@ -1,27 +1,26 @@
-const {readFileSync} = require('fs');
 const {execSync} = require("child_process");
 
 const getLibraries = () => {
-	const items = [];
-	const data = JSON.parse(readFileSync('angular.json')).projects;
-	const keys = Object.keys(data);
-	Object.values(data).forEach((value, key) => {
-		if (value.projectType == 'library')
-			items.push({key: keys[key], value: value});
-	})
-	return items;
+	return [
+		'core',
+		'components',
+		'auth',
+		'form-builder',
+		'components-bootstrap',
+		'form-builder-bootstrap',
+	];
 }
 for (let library of getLibraries()) {
 	try {
-		console.log(`start building [${library.key}] ...`)
-		execSync(`ng build --project ${library.key} --prod`)
+		console.log(`start building [${library}] ...`)
+		execSync(`ng build --project ${library} --prod`)
 		if (process.argv.includes('--install')) {
-			console.log(`npm install [${library.key}] ...`)
+			console.log(`npm install [${library}] ...`)
 			execSync(`npm i`)
 		}
 		if (process.argv.includes('--publish')) {
-			console.log(`publishing package [${library.key}] ...`)
-			execSync(`npm publish dist/${library.key} --access public`)
+			console.log(`publishing package [${library}] ...`)
+			execSync(`npm publish dist/${library} --access public`)
 		}
 	} catch (e) {
 		console.error(e);
