3c14e401c79d74fbc8230510cd0e994f71327afa
mo3in <moein.hente@gmail.com>
2020-09-06 12:07:42 +0430

    input hidden & date-time

--- projects/components/input/src/directive/kinput-text/kinput-text.directive.ts
+++ projects/components/input/src/directive/kinput-text/kinput-text.directive.ts
@@ -1,13 +1,13 @@
 import {Directive, Input} from '@angular/core';
 import {FormControl, FormControlDirective} from '@angular/forms';
 
-export class InputDirectiveBase extends FormControlDirective {
-}
-
-@Directive({
-	selector: 'k-input-text'
-})
-export class KInputTextDirective extends InputDirectiveBase {
-
-}
-
+// export class InputDirectiveBase extends FormControlDirective {
+// }
+//
+// @Directive({
+// 	selector: 'k-input-text'
+// })
+// export class KInputTextDirective extends InputDirectiveBase {
+//
+// }
+//
--- projects/components/input/src/k-input.module.ts
+++ projects/components/input/src/k-input.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { KInputTextComponent } from './components/k-input-text/k-input-text.component';
-import { KInputDateComponent } from './components/kinput-date/k-input-date.component';
-import { KInputTextDirective } from './directive/kinput-text/kinput-text.directive';
-import { TestComponent } from './test/test.component';
-
+// import {KInputTextDirective} from './directive/kinput-text/kinput-text.directive';
 
 @NgModule({
-	declarations: [KInputTextComponent, KInputDateComponent, KInputTextDirective, TestComponent],
+	declarations: [
+		// KInputTextDirective
+	],
 	imports: [
 		CommonModule
 	]
